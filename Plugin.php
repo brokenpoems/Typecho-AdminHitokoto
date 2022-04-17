1ad9b0a451ffc6e0fe78653ae246f76c2389dfec
brokenpoems <brokenpoems@foxmail.com>
2022-04-18 03:35:44 +0800

    v1.0.0

---
+++ Plugin.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * AdminHitokoto
+ *
+ * @package AdminHitokoto
+ * @author brokenpoems
+ * @version 1.0.0
+ * @link https://github.com/brokenpoems/Typecho-Admin-hitokoto
+ */
+
+class AdminHitokoto_Plugin implements Typecho_Plugin_Interface
+{
+    /* 激活插件方法 */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('admin/menu.php')->navBar = __CLASS__ . '::render';
+    }
+    
+    /* 禁用插件方法 */
+    public static function deactivate()
+    {
+    }
+    /**
+     *
+     * a   动画
+     * b   漫画
+     * c   游戏
+     * d   文学
+     * e   原创
+     * f   来自网络
+     * g   其他
+     * h   影视
+     * i   诗词
+     * j   网易云
+     * k   哲学
+     * l   抖机灵
+     */
+    /* 插件配置方法 */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        // 一言API设置
+        $api_url = new Typecho_Widget_Helper_Form_Element_Text('api_url', NULL, 'https://v1.hitokoto.cn/', _t('一言API'));
+        $form->addInput($api_url);
+        
+        // 句子类型设置
+        $sentence_type = new Typecho_Widget_Helper_Form_Element_Checkbox('sentence_type', array(
+            'a' => _t('动画'),
+            'b' => _t('漫画'),
+            'c' => _t('游戏'),
+            'd' => _t('文学'),
+            'e' => _t('原创'),
+            'f' => _t('来自网络'),
+            'g' => _t('其他'),
+            'h' => _t('影视'),
+            'i' => _t('诗词'),
+            'j' => _t('网易云'),
+            'k' => _t('哲学'),
+            'l' => _t('抖机灵')
+        ), array(
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l'
+        ), _t('句子类型'), _t('如果什么都没选按照v1的api请求是全选的意思'));
+        $form->addInput($sentence_type);
+    }
+    
+    /* 个人用户的配置方法 */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
+    
+    /* 插件实现方法 */
+    public static function render()
+    {
+        $plugin = Typecho_Widget::widget('Widget_Options')->Plugin('AdminHitokoto');
+        $apiurl = $plugin->api_url . '?encode=text';
+        if ($plugin->sentence_type) {
+            if (is_array($plugin->sentence_type)) {
+                $sentence = $plugin->sentence_type;
+                foreach ($sentence as $opt) {
+                    $apiurl = $apiurl . '&c=' . $opt;
+                }
+            }
+        }
+        $hitokoto = file_get_contents($apiurl);
+        
+        echo '<span class="message success">' . $hitokoto . '</span>';
+        //echo $apiurl;
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Typecho-Admin-hitokoto
-为博客后台添加一言,魔改于Hello Wolrd,基于一言API
+# Typecho-AdminHitokoto
+为博客后台添加一言,~~魔改于Hello Wolrd~~,基于一言API
+
+## 食用方法
+1. 法1
+  1. 从仓库首页 => code => get zip => 解压
+  2. RENAME AdminHitokoto
+  3. 配置开始食用吧
+2. 法2
+  1. 从Releases处获取
+  2. 之后同法1
+
+# Attention!:
+1. 目录结构应该为/站点目录/usr/plugin/AdminHitokoto/Plugin.php(否则会加载出错)
+
+# 发现bug?
+提交issue或在[我的Blog](https://brokenpoems.cf/)上评论留言
